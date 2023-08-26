0ae71e07dd6f920f20df1b9627273770e17db609
StartDeveloperKim <ktw550111@naver.com>
2023-08-26 13:57:38 +0900

    #추가 : 시작페이지 Activity 및 코드

---
+++ app/src/main/java/com/project/pill_so_good/layout/MainActivity.java
@@ -0,0 +1,4 @@
+package com.project.pill_so_good.layout;
+
+public class MainActivity {
+}
---
+++ app/src/main/java/com/project/pill_so_good/layout/StartActivity.java
@@ -0,0 +1,33 @@
+package com.project.pill_so_good.layout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.pill_so_good.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        startLoading();
+    }
+    
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
